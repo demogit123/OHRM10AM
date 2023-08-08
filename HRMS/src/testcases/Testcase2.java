d3261d1f41ad86463e7263620111431921096622
MAXHUB <MAXHUB@DESKTOP-0RQF8FK>
2023-08-08 11:15:43 +0530

    testcase2 updated....

--- HRMS/src/testcases/Testcase2.java
+++ HRMS/src/testcases/Testcase2.java
@@ -1,10 +1,11 @@
 package testcases;
 
-public class testcase2 {
+public class Testcase2 {
 
 	public static void main(String[] args) 
 	{
 		System.out.println("This is testcase 2");
+		System.out.println("TestCase 2 updated...");
 	}
 
 }
